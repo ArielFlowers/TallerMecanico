7d1f270d85e58c40c967ab34ddd7862fa4786d95
ArielFlowers <arielmakotoflores@gmail.com>
2026-08-31 18:21:42 -0400

    feat: crear proyecto inicial Razor Pages

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TallerMecanico.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
