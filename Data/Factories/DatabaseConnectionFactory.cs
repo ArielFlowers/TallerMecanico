a1f671156228d73e60c175703be6da80733ede82
aldair <aldair.diaz@ucb.edu.bo>
2026-09-23 09:58:13 -0400

    refactor(data): agregar factory de conexion MySQL

---
+++ Data/Factories/DatabaseConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+
+namespace TallerMecanico.Data.Factories;
+
+public abstract class DatabaseConnectionFactory
+{
+    protected readonly string ConnectionString;
+
+    protected DatabaseConnectionFactory(IConfiguration configuration)
+    {
+        ConnectionString =
+            configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException(
+                "La cadena de conexión no está configurada.");
+    }
+
+    public abstract DbConnection CreateConnection();
+}
---
+++ Data/Factories/MySqlConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using MySqlConnector;
+
+namespace TallerMecanico.Data.Factories;
+
+public sealed class MySqlConnectionFactory : DatabaseConnectionFactory
+{
+    public MySqlConnectionFactory(IConfiguration configuration)
+        : base(configuration)
+    {
+    }
+
+    public override DbConnection CreateConnection()
+    {
+        return new MySqlConnection(ConnectionString);
+    }
+}
